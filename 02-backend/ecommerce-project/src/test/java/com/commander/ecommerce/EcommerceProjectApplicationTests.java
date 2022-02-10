58c0ca0c9c5a0ef325c33cf11ae777310f7d8475
tamir <tamirham@gmail.com>
2022-02-10 09:41:11 +0200

    eComerce-project-2.0

---
+++ 02-backend/ecommerce-project/src/test/java/com/commander/ecommerce/EcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commander.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
